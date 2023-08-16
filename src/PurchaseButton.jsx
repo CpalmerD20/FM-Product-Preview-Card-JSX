da1ce897fc598f469a410f9fe13f4f6d1f171677
Cheyne Palmer <palmerc@MBP.home>
2023-08-15 18:50:52 -0700

    update to CSS: add-to-cart button

--- src/PurchaseButton.jsx
+++ src/PurchaseButton.jsx
@@ -10,7 +10,7 @@ export default function PurchaseButton({ salePrice, fullPrice }) {
     <button
       className="add-to-cart"
       onClick={handleClick}
-    ><img src="./src/assets/icon-cart.svg" alt="" />Add to Cart
+    ><img className="cart-icon" src="./src/assets/icon-cart.svg" alt="" />Add to Cart
     </button>
   </>
 }
