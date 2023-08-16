d84629f84877fc8bf52957537c87c77daa33e147
Cheyne Palmer <palmerc@MBP.home>
2023-08-15 18:07:09 -0700

    add: update to github workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./FM-Product-Preview-Card-JSX/",
   plugins: [react()],
+  base: "/FM-Product-Preview-Card-JSX/",
 })
