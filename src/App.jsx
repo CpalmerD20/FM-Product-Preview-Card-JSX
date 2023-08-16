6f987dcef28e0fdd074669462ca328ec5ed2848f
Cheyne Palmer <palmerc@MBP.home>
2023-08-15 19:25:09 -0700

    another kitchen sink to get images to render on GH pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 import CardWrapper from './CardWrapper';
 
 const productConfig = {
-  desktopView: "./src/assets/product-desktop.jpg",
-  mobileView: "./src/assets/product-mobile.jpg",
+  desktopView: "./public/assets/product-desktop.jpg",
+  mobileView: "./public/assets/product-mobile.jpg",
   type: "PERFUME",
   name: "Gabrielle Essence Eau De Parfum",
   description: "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
